8bfec2fa0b055e2bc211d2e442c242e1b734906b
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2022-06-30 12:05:12 -0400

    Initial commit

---
+++ Etiquetas com Glue/script.R
@@ -0,0 +1,18 @@
+
+# Pacotes -----------------------------------------------------------------
+
+library(tidyverse)
+library(glue)
+
+
+# Procedimento ------------------------------------------------------------
+
+tratamento <- c("Ctrl","A","B","C")
+tempo <- c(1,2,3,4,5)
+experimentos <- c("DNA","RNA","Proteina")
+
+
+tidyr::crossing(tratamento,tempo,experimentos) %>% 
+  dplyr::mutate(Label = glue("Droga: {tratamento} - {tempo}  dias - {experimentos}")) %>% 
+  dplyr::select(Label) %>% 
+  readr::write_csv(file = "etiquetas.csv",col_names = F)
